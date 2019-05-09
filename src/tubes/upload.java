d2cdd80b578734c5a5d900664b241ba662928593
IrfaniYahya <yahyairfani1@gmail.com>
2019-05-09 22:50:03 +0800

    Fiturlogin

---
+++ src/tubes/upload.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubes;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+/**
+ *
+ * @author Personal
+ */
+public class upload   {
+    
+    
+}
